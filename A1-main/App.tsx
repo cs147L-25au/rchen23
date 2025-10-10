096ebcbd5d237a272f1a12956fd53a2c57d14f7f
rrchen <rchen23@stanford.edu>
2025-10-10 02:55:05 -0700

    Header + light/darkMode Pressable
    Created the header section using flexBoxes and nested flexboxes. Text was put in with the fonts according to the instruction. Made sure the light/dark mode image became a pressable button that could swap upon being pressed.

--- A1-main/App.tsx
+++ A1-main/App.tsx
@@ -1,13 +1,22 @@
 /**
  * You don't need to edit this file, but you can if you want!
  * Almost of the work for this assignment is in Header.js, Body.js, and Footer.js.
- * 
+ *
  * If you're doing the light/dark mode toggle extension, you will need to make
  * a couple tweaks here.
  */
 
 import { useCallback } from "react";
-import { StyleSheet, View, StatusBar } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  Text,
+  Image,
+  Pressable,
+  Button,
+} from "react-native";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
@@ -21,6 +30,8 @@ import Footer from "./app/components/Footer";
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  // move usedState to the top
+  const [darkMode, setDarkMode] = useState(0);
 
   /* BEGIN FONT LOADING CODE -- You don't need to touch this section unless you want to. */
   const [fontsLoaded] = useFonts({
@@ -38,11 +49,36 @@ export default function App() {
   /* END FONT LOADING CODE */
 
   // If you want to use dark mode, change this accordingly.
+
+  // Header Items:
   StatusBar.setBarStyle(Themes.light.statusBar);
+  const userName: string = "James Landay";
+  const userPronouns: string = "he/him/his";
+
+  const Modes = [
+    require("./assets/Icons/sun.png"),
+    require("./assets/Icons/moon.png"),
+  ];
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <Header />
+      <View style={styles.Header}>
+        <View style={styles.textContainer}>
+          <Text style={styles.Name}>{userName}</Text>
+          <Text style={styles.Pronouns}>{userPronouns}</Text>
+        </View>
+        <Pressable
+          onPress={() => {
+            if (darkMode == 0) {
+              setDarkMode(1);
+            } else {
+              setDarkMode(0);
+            }
+          }}
+        >
+          <Image style={styles.modeButton} source={Modes[darkMode]} />
+        </Pressable>
+      </View>
       <Body />
       <Footer />
     </View>
@@ -56,5 +92,34 @@ const styles = StyleSheet.create({
     backgroundColor: Themes.light.bg,
     alignItems: "center",
     paddingHorizontal: 16,
+  },
+  Header: {
+    display: "flex",
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textContainer: {
+    flexDirection: "column", // stack the name over pronoun vertically
+    alignItems: "flex-start", // aligns top text to left of screen
+  },
+  modeButton: {
+    height: 40,
+    width: 40,
+    margin: 4,
+  },
+  Name: {
+    fontSize: 32,
+    fontFamily: "Sydney-Bold",
+    textAlign: "left",
+  },
+  Pronouns: {
+    fontSize: 16,
+    fontFamily: "Sydney",
+    textAlign: "left",
   },
 });
